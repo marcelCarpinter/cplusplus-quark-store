97af633b4283b85e7ca54f5eef2fd339b7e41f4d
marcelCarpinter <marcel.carpinter.1995@gmail.com>
2023-01-11 13:09:51 -0300

    Updated Seller. Added Getters

--- Model/Seller.cpp
+++ Model/Seller.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "Seller.h"
+
+Seller::Seller(string name, string lastname, string code) {
+    this->name = name;
+    this->lastname = lastname;
+    this->code = code;
+}
+
+Seller::~Seller() {
+
+}
+
+string Seller::getName() {
+    return this->name;
+}
+
+string Seller::getLastname() {
+    return this->lastname;
+}
+
+string Seller::getCode() {
+    return this->code;
+}
--- Model/Seller.h
+++ Model/Seller.h
@@ -1,13 +1,20 @@
-//
-// Created by Marcel on 11/1/2023.
-//
-
 #ifndef MAYORISTA_SELLER_H
 #define MAYORISTA_SELLER_H
 
+using namespace std;
+#include <string>
 
 class Seller {
-
+private:
+    string name;
+    string lastname;
+    string code;
+public:
+    Seller(string, string, string);
+    ~Seller();
+    string getName();
+    string getLastname();
+    string getCode();
 };
 
 
