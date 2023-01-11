6ff14ab40135a31bcf341078efdfd1bafd94a2d4
marcelCarpinter <marcel.carpinter.1995@gmail.com>
2023-01-11 14:01:05 -0300

    Started Store hpp declarations

--- Model/Store.hpp
+++ Model/Store.hpp
@@ -5,9 +5,16 @@
 #ifndef MAYORISTA_STORE_HPP
 #define MAYORISTA_STORE_HPP
 
+using namespace std;
+#include <string>
 
 class Store {
-
+private:
+    string name;
+    string address;
+public:
+    Store(string, string);
+    ~Store();
 };
 
 
