cc90dea136f94a135acdb69652af5fbb52d78ca7
marcelCarpinter <marcel.carpinter.1995@gmail.com>
2023-01-11 13:01:03 -0300

    Added Seller entity

---
+++ Model/Seller.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Marcel on 11/1/2023.
+//
+
+#include "Seller.h"
---
+++ Model/Seller.h
@@ -0,0 +1,14 @@
+//
+// Created by Marcel on 11/1/2023.
+//
+
+#ifndef MAYORISTA_SELLER_H
+#define MAYORISTA_SELLER_H
+
+
+class Seller {
+
+};
+
+
+#endif //MAYORISTA_SELLER_H
