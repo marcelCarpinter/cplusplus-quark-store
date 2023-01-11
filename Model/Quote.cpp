b6cd56dc2d39ed8f9f3d3767566c6cf8392c2fc2
marcelCarpinter <marcel.carpinter.1995@gmail.com>
2023-01-11 13:10:19 -0300

    Updated Quote. Added Constructor

--- Model/Quote.cpp
+++ Model/Quote.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "Quote.hpp"
+
+Quote::Quote(string id, string createdDate, string createdTime, string sellerCode, string itemId, int quantity) {
+    this->id = id;
+    this->createdDate = createdDate;
+    this->createdTime = createdTime;
+    this->sellerCode = sellerCode;
+    this->itemId = itemId;
+    this->quantity = quantity;
+}
---
+++ Model/Quote.hpp
@@ -0,0 +1,25 @@
+//
+// Created by Marcel on 11/1/2023.
+//
+
+#ifndef MAYORISTA_QUOTE_HPP
+#define MAYORISTA_QUOTE_HPP
+
+using namespace std;
+#include <string>
+
+class Quote {
+private:
+    string id;
+    string createdDate;
+    string createdTime;
+    string sellerCode;
+    string itemId;
+    int quantity;
+public:
+    Quote(string, string, string, string, string, int);
+    ~Quote();
+};
+
+
+#endif //MAYORISTA_QUOTE_HPP
