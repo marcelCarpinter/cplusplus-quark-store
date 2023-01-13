c1417283b8a80b01d2fce12b4f5270109086525e
marcelCarpinter <marcel.carpinter.1995@gmail.com>
2023-01-13 15:36:50 -0300

    Added attributes and methods to Pant class

--- Model/Clothes/Pant.cpp
+++ Model/Clothes/Pant.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "Pant.hpp"
+
+Pant::Pant(string quality, int price, int qty, bool normal) : Clothes(quality, price, qty) {
+    this->normal = normal;
+}
+
+Pant::~Pant() {
+
+}
+
+bool Pant::isNormal() {
+    return this->normal;
+}
--- Model/Clothes/Pant.hpp
+++ Model/Clothes/Pant.hpp
@@ -6,8 +6,15 @@
 #define MAYORISTA_PANT_HPP
 
 
-class Pant {
+#include "../Clothes.hpp"
 
+class Pant: public Clothes {
+private:
+    bool normal;
+public:
+    Pant(string unnamed, int unnamed1, int unnamed2, bool);
+    ~Pant();
+    bool isNormal();
 };
 
 
