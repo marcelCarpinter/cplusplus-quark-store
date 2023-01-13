803b1d18af55ccb4c8baa841737cc735f69b9e08
marcelCarpinter <marcel.carpinter.1995@gmail.com>
2023-01-13 15:28:11 -0300

    Pant class added

---
+++ Model/Clothes/Pant.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Marcel on 13/1/2023.
+//
+
+#include "Pant.hpp"
---
+++ Model/Clothes/Pant.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Marcel on 13/1/2023.
+//
+
+#ifndef MAYORISTA_PANT_HPP
+#define MAYORISTA_PANT_HPP
+
+
+class Pant {
+
+};
+
+
+#endif //MAYORISTA_PANT_HPP
