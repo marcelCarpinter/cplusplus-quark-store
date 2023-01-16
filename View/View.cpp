71d3ca32af1d3defa5794485bd8e297d65e62f15
marcelCarpinter <marcel.carpinter.1995@gmail.com>
2023-01-16 15:54:11 -0300

    Adding methods to display store and seller info on console

--- View/View.cpp
+++ View/View.cpp
@@ -2,4 +2,30 @@
 // Created by Marcel on 14/1/2023.
 //
 
+#include <iostream>
 #include "View.hpp"
+
+View::View() {
+    this->m_presenter = new Presentation(this);
+    this->showText("Cotizador Express - Menú Principal");
+    this->showText("-----------------------------");
+    this->showStoreInfo();
+    this->showText("-----------------------------");
+    this->showSellerInfo();
+}
+
+View::~View() {
+
+}
+
+void View::showText(const string &text) {
+    std::cout << text << std::endl;
+}
+
+void View::showStoreInfo() {
+    this->showText(this->m_presenter->getStoreInfo());
+}
+
+void View::showSellerInfo(){
+    this->showText("Nombre de Vendedor | Código de Vendedor");
+}
--- View/View.hpp
+++ View/View.hpp
@@ -5,9 +5,25 @@
 #ifndef MAYORISTA_VIEW_HPP
 #define MAYORISTA_VIEW_HPP
 
+#include "../Interface/IView.hpp"
+#include "../Presentation/Presentation.hpp"
+// #include "../Presentation/Presentation.hpp"
 
-class View {
+using namespace std;
+#include <string>
 
+
+class View: public IView {
+private:
+    Presentation* m_presenter;
+    void showStoreInfo();
+    void showSellerInfo();
+    //void showMainMenu();
+    //void readOption(const char* option, bool& exitCondition);
+public:
+    View();
+    ~View() override;
+    void showText(const std::string& text) override;
 };
 
 
